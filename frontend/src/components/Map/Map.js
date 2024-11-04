5209b9166437c7dbd974e7ab5dca53259a1edbd7
sriharinewuser <sri948653@gmail.com>
2024-11-04 01:46:17 -0500

    update

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import classes from './map.module.css';
 import 'leaflet/dist/leaflet.css';
@@ -6,7 +7,7 @@ import {
   TileLayer,
   Marker,
   Popup,
-  useMap,
+  useMapEvents,
 } from 'react-leaflet';
 import { toast } from 'react-toastify';
 import * as L from 'leaflet';
@@ -39,7 +40,6 @@ export default function Map({ readonly, location, onChange }) {
 
 function FindButtonAndMarker({ readonly, location, onChange }) {
   const [position, setPosition] = useState(location);
-  const map = useMap();
 
   useEffect(() => {
     if (readonly) {
@@ -49,17 +49,17 @@ function FindButtonAndMarker({ readonly, location, onChange }) {
     if (position) onChange(position);
   }, [position]);
 
-  map.on('click', e => {
-    !readonly && setPosition(e.latlng);
-  });
-
-  map.on('locationfound', e => {
-    setPosition(e.latlng);
-    map.flyTo(e.latlng, 13);
-  });
-
-  map.on('locationerror', e => {
-    toast.error(e.message);
+  const map = useMapEvents({
+    click(e) {
+      !readonly && setPosition(e.latlng);
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, 13);
+    },
+    locationerror(e) {
+      toast.error(e.message);
+    },
   });
 
   const markerIcon = new L.Icon({
@@ -98,119 +98,3 @@ function FindButtonAndMarker({ readonly, location, onChange }) {
     </>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import classes from './map.module.css';
-// import 'leaflet/dist/leaflet.css';
-// import {
-//   MapContainer,
-//   TileLayer,
-//   Marker,
-//   Popup,
-//   useMapEvents,
-// } from 'react-leaflet';
-// import { toast } from 'react-toastify';
-// import * as L from 'leaflet';
-
-// export default function Map({ readonly, location, onChange }) {
-//   return (
-//     <div className={classes.container}>
-//       <MapContainer
-//         className={classes.map}
-//         center={[0, 0]}
-//         zoom={1}
-//         dragging={!readonly}
-//         touchZoom={!readonly}
-//         doubleClickZoom={!readonly}
-//         scrollWheelZoom={!readonly}
-//         boxZoom={!readonly}
-//         keyboard={!readonly}
-//         attributionControl={false}
-//       >
-//         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-//         <FindButtonAndMarker
-//           readonly={readonly}
-//           location={location}
-//           onChange={onChange}
-//         />
-//       </MapContainer>
-//     </div>
-//   );
-// }
-
-// function FindButtonAndMarker({ readonly, location, onChange }) {
-//   const [position, setPosition] = useState(location);
-
-//   useEffect(() => {
-//     if (readonly) {
-//       map.setView(position, 13);
-//       return;
-//     }
-//     if (position) onChange(position);
-//   }, [position]);
-
-//   const map = useMapEvents({
-//     click(e) {
-//       !readonly && setPosition(e.latlng);
-//     },
-//     locationfound(e) {
-//       setPosition(e.latlng);
-//       map.flyTo(e.latlng, 13);
-//     },
-//     locationerror(e) {
-//       toast.error(e.message);
-//     },
-//   });
-
-//   const markerIcon = new L.Icon({
-//     iconUrl: '/marker-icon-2x.png',
-//     iconSize: [25, 41],
-//     iconAnchor: [12.5, 41],
-//     popupAnchor: [0, -41],
-//   });
-
-//   return (
-//     <>
-//       {!readonly && (
-//         <button
-//           type="button"
-//           className={classes.find_location}
-//           onClick={() => map.locate()}
-//         >
-//           Find My Location
-//         </button>
-//       )}
-
-//       {position && (
-//         <Marker
-//           eventHandlers={{
-//             dragend: e => {
-//               setPosition(e.target.getLatLng());
-//             },
-//           }}
-//           position={position}
-//           draggable={!readonly}
-//           icon={markerIcon}
-//         >
-//           <Popup>Shipping Location</Popup>
-//         </Marker>
-//       )}
-//     </>
-//   );
-// }
